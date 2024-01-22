33eeceed4dab720fcdac63333e83fe08db5e6477
cjephuneh <calebjephuneh@gmail.com>
2024-01-22 13:09:29 +0300

    done with the design

--- components/accelerate.tsx
+++ components/accelerate.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Hero = () => {
   return (
     <div className="relative bg-cover bg-center bg-no-repeat py-[120px]" style={{ backgroundImage: 'url(/6.png)' }}>
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-30"></div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></div>
       <div className="container mx-auto px-6 md:px-12 xl:px-32 relative z-10">
         <div className="text-center">
           <h1 className="text-3xl md:text-3xl xl:text-3xl font-bold text-cTtext mb-4">
--- components/cta.tsx
+++ components/cta.tsx
@@ -2,7 +2,7 @@ const Hero = () => {
   return (
     <div className="relative bg-cover bg-center bg-no-repeat py-[200px]" style={{ backgroundImage: 'url(/9.png)' }}>
       {/* This div is the overlay; it's absolutely positioned and should not affect the clarity of the text */}
-      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-30"></div>
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-black opacity-70"></div>
 
       {/* This container holds the text and is relative to the Hero div, so it's on top of the overlay. The text here should be clear. */}
       <div className="container mx-auto px-6 md:px-12 xl:px-32 relative z-10"> {/* Ensure this is above the overlay with z-10 */}
